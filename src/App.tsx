1039f30e45bdd0aad065e297495f5ea29d5137ca
Suracha2550 <86565283+PPSK2550@users.noreply.github.com>
2023-04-04 13:20:53 +0700

    change Gallery route path

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path=":search" element={<Gallery />} />
+          <Route path="/search/:query" element={<Gallery />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
