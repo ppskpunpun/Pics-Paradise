e0ecd83745cbe2b1e83bd69af7597a034bb55a5b
Suracha2550 <86565283+PPSK2550@users.noreply.github.com>
2023-04-08 19:38:24 +0700

    merge infinite-scroll to master

--- src/components/PicsColumn.tsx
+++ src/components/PicsColumn.tsx
@@ -5,7 +5,7 @@ export default function PicsColumn({ images }: { images: Array<imageData> }) {
     return (
         <div className="pics-column">
             {images.map((image, index) => {
-                return <Pic key={index + image.title} image={image} />
+                return <Pic key={index + image.description} image={image} />
             })}
         </div>
     )
