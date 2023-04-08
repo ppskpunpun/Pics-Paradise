dbcd0cee027ccbfe5ae5449af8cf6fd6d6b2e6e8
Suracha2550 <86565283+PPSK2550@users.noreply.github.com>
2023-04-08 18:20:43 +0700

    add formatData function

--- src/components/Pictures.tsx
+++ src/components/Pictures.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useFetch from '../hooks/useFetch'
 import PicsColumn from './PicsColumn'
 import { imageData, formatUnsplashResult } from './formatUnsplashResult'
@@ -11,36 +11,43 @@ export default function Pictures({ search='' }) {
     //     `https://api.unsplash.com/search/photos/?client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}&query=${search}&page=${page}&per_page=30` 
     //     : `https://api.unsplash.com/photos/random/?client_id=${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}&count=30`
 
-    // // placeholder API
+    // placeholder API
     const url = search != '' ?
         'https://worrisome-baseball-cap-hare.cyclic.app/api/unsplash/placeholder/tokyo'
         : 'https://worrisome-baseball-cap-hare.cyclic.app/api/unsplash/placeholder/random'
 
     const { isLoading, data, error } = useFetch(url)
-
-    let imagesData: imageData[] = []
-
-    if (data) {
-        imagesData = search != ''
-        ? data.results.map((result: any): imageData => {
-            return formatUnsplashResult(result)
-        })
-        : data.map((result: any): imageData => {
-            return formatUnsplashResult(result)
-        })
-    }
-
+    
     if (isLoading) return <div>Loading...</div>
 
     if (error) return <div>Opps! something went wrong.</div>
 
-    return (
-        <section className="pictures-section">
-            {countingOffSepArray(imagesData, 3).map((images: imageData[], index) => {
-                return <PicsColumn key={index + 'picscolumn'} images={images} />
-            })}
-        </section>
-    )
+    if (data) {
+        return (
+            <section className="pictures-section">
+                {
+                    countingOffSepArray(formatData(data, search != ''), 3)
+                    .map((images: imageData[], index) => {
+                        return <PicsColumn key={index + 'picscolumn'} images={images} />
+                    })
+                }
+            </section>
+        )
+    }
+}
+
+function formatData(data: any, isSearch: boolean) {
+    let imagesData: imageData[] = []
+
+    imagesData = isSearch
+    ? data.results.map((result: any): imageData => {
+        return formatUnsplashResult(result)
+    })
+    : data.map((result: any): imageData => {
+        return formatUnsplashResult(result)
+    })
+
+    return imagesData
 }
 
 function countingOffSepArray(arr: Array<any>, countTo: number) {
