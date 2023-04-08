ffe346c95111cb7da8db10af2808afa94d4d3467
Suracha2550 <86565283+PPSK2550@users.noreply.github.com>
2023-04-08 14:35:56 +0700

    picture info width resizable

--- src/components/Pic.tsx
+++ src/components/Pic.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useLayoutEffect } from 'react'
 import { createPortal } from 'react-dom'
 
 import PicView from './PicView'
@@ -9,8 +9,9 @@ import closeIcon from '../assets/close-icon.svg'
 
 export default function Pic({ image }: { image: imageData }) {
 
+    const ref = useRef<any>(null) // image element
+    const [width, setWidth] = useState(0) // width of image use to set info width
     const [showDetail, setShowDetail] = useState(false)
-
     const [showPopUp, setShowPopUp] = useState(false)
 
     const onMouseEvents = {
@@ -18,14 +19,22 @@ export default function Pic({ image }: { image: imageData }) {
         onMouseLeave: () => setShowDetail(false)
     }
 
+    useLayoutEffect(() => {
+        setWidth(ref.current?.offsetWidth)
+    }, [])
+
+    window.addEventListener('resize', () => {
+        setWidth(ref.current?.offsetWidth)
+    })
+
     return (
         <div className="picture-card">
 
-            <img src={image.url} alt={image.title} {...onMouseEvents} style={showDetail ? {filter: 'brightness(.65)'} : {} }
+            <img ref={ref} src={image.url} alt={image.title} {...onMouseEvents} style={showDetail ? {filter: 'brightness(.65)'} : {} }
                 onClick={() => setShowPopUp(true)}
             />
             
-            <div className="info" style={{ opacity: showDetail ? '1' : '0' }} {...onMouseEvents}>
+            <div className="info" style={{ opacity: showDetail ? '1' : '0', width: `${width}px` }} {...onMouseEvents}>
                 <a href={image.user.link} target='_blank'>
                     <img src={image.user.profile_image} alt={`profile picture of ${image.user.name}`} />
                 </a> 
