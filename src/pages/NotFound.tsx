9135664b42127f302c2cbe0fc1e474b3d4a4f70a
Suracha2550 <86565283+PPSK2550@users.noreply.github.com>
2023-04-09 13:19:13 +0700

    make not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+import poopIcon from '../assets/poop-icon.svg'
+
 export default function NotFound() {
+
+    const navigate = useNavigate()
+
     return (
-        <></>
+        <div className="not-found">
+            <img src={poopIcon} alt="Shit!" width="256" />
+            <h1>404</h1>
+            <p>What are you looking for? Page not found.</p>
+            <button onClick={() => navigate('/')}>Back To Home</button>
+        </div>
     )
 }
